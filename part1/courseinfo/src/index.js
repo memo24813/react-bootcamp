e09594f749f4aa8b33278f25910c6d55a2bde95c
memo24813 <eugenio.gutierrez@uabc.edu.mx>
2021-01-31 20:27:37 -0800

    part 1 bootcamp

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import ReactoDOM from 'react-dom'
+
+
+// Header component
+const Header = (props) =>{
+  return(
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+// Content component
+const Content = (props) =>{
+  return(
+    <>
+      <p>{props.part} {props.exercise}</p>
+    </>
+  )
+}
+
+// Number exercises component
+
+const Total = (props) =>{
+  const numbers = props.total
+  const sum = numbers.reduce((a,b) => a + b,0)
+  return (
+    <>
+      <p>Number of excercises {sum}</p>
+    </>
+  )
+}
+
+const App = () =>{
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content part = {part1} exercise = {exercises1}/>
+      <Content part = {part2} exercise = {exercises2}/>
+      <Content part = {part3} exercise = {exercises3}/>
+      <Total total={[exercises1,exercises2,exercises3]} />
+    </div>
+  )
+}
+ReactoDOM.render(<App/>,document.getElementById('root'))
